ad3625dd5832bba67868e7f4c2c107864edf80de
Bart <bartstorm1@gmail.com>
2019-09-25 14:16:52 +0200

    test

---
+++ gAmE1.js
@@ -0,0 +1,102 @@
+alert("Je moeder is boos en zegt dat je de afwas moet doen.");
+var keus1 = prompt("1 Je pakt de fiets en vlucht.  2 Je doet de afwas en je wilt je moeder blij maken");
+if(keus1 == "1"){
+	alert("Je bent veilig weg en je hoeft de afwas niet te doen.");
+  	alert("Je stopt bij een kruispunt en er staat er auto aan de rechterkant.");
+   	var keus2 = prompt("1. Je pakt een steen en gooit die tegen de autobestuurder 2. Je wacht totdat al het verkeer weg is.");
+   	
+   	if(keus2 == "2"){
+  		alert("Je hebt je verkeersdiploma gehaald, dus je wist hoe je goed moest handelen. Het is goed afgelopen.");
+  		alert("Je hebt trek en je gaat naar de MacDonalds. Je ziet dat er een overval bezig is.")
+   		var keus3 = prompt("1 Je hebt zin in een BigMac en je bestelt er een. 2 Je denkt dat je slim bent en je overvalt de overvallers.")
+   		
+   		if(keus3 == "2"){
+  			alert("De overvallers hebben je door, maar ze snappen het. Dus ze geven je een breekijzer.")
+  			alert("De trek is veranderd naar honger en je ziet een zwerver op de grond zitten met een half opgegeten salade van de Albert Heijn.")
+   			var keus4 = prompt("1 Je bedreigt de zwerver met een breekijzer en je vlucht met de salade. 2 Je laat de zwerver liggen, want de salade ziet er niet hygiënisch uit.")
+   
+   			if(keus4 == "1"){
+ 				alert("De zwerver loopt mank en je hebt succesvol de salade gestolen!")
+ 				alert("Je loopt over straat en ziet in de verte Ome Dirk. Hij is je nog 15 euro schuldig.")
+ 				var keus5 = prompt("1 Je slaat hem op zijn hoofd met het breekijzer en steelt zijn geld. 2 Je geeft de vieze salade aan Dirk om hem zo te vergiftigen.")
+
+ 				if(keus5 == "1"){
+ 					alert("Je hebt zojuist Ome Dirk neergeslagen, alleen hij heeft geen geld opzak.")
+ 					alert("Je belt 112 voor Ome Dirk, want je wilt hem niet zo achterlaten. Na 15 minuten arriveert de ambulance.")
+ 					var keus6 = prompt("1 Je overvalt de ambulance en je steelt hun auto. 2 Je zegt dat hij is overvallen en je rijdt mee naar het ziekenhuis met de ambulancebroeders.")
+
+ 					if(keus6 == "2"){
+ 						alert("Je arriveert veilig bij het ziekenhuis en je krijgt een dropje voor de schrik.")
+ 						alert("Het is lekker warm in het ziekenhuis en je struint door het gebouw. Totdat je bij de kantine arriveert. Er staan veel lekkere broodjes.")
+ 						var keus7 = prompt("1 Je pakt een broodje en stopt hem in je broek. 2 Je verkleed jezelf als een kantine medewerker en je pakt broodjes.")
+
+ 						if(keus7 == "1"){
+ 							alert("Je bent niet gezien en het broodje zit daar wel prima.")
+ 							alert("Je gaat door met struinen door het gebouw. Je komt op de bovenste etage van het gebouw en een deur staat open. Je loopt erdoor heen en je bent gearriveerd bij het helikopter platvorm. Er staan 2 helikopters. ")
+ 							var keus8 = prompt("1 Helikopters zijn gaaf dus je gaat erin zitten en je zet hem aan. Je hebt veel GTA gespeeld dus je weet hoe zo’n ding werkt. 2 Het is een grote kans dat je dit niet overleeft en je gaat weer naar beneden.")
+
+ 							if(keus8 == "1"){
+ 								alert("Het is niet zo moeilijk als het lijkt en je vlucht met de helikopter weg. Je vliegt mooi boven de stad.")
+ 								alert("Er zat heel weinig brandstof in de helikopter en je moet een noodlanding maken.")
+ 								var keus9 = prompt("1 Er staat een groot grasveld waar je kan landen. 2 Je kan landen op het dak van de kringloop winkel.")
+
+ 								if(keus9 == "2"){
+ 									alert("Je landt veilig op het dak en je loopt naar binnen. Je biedt de helikopter aan en ze bieden er 20.000,- voor.")
+ 									alert("20.000,- is veel geld. Je loopt naar de juwelier en je wilt een mooie ketting kopen.")
+ 									var keus10 = prompt("1 Je kan een mooie ketting kopen en je krijgt er een horloge gratis bij. 2 Je hebt geen zin in onderhandelen en je overvalt de juwelier met je breekijzer.")
+
+ 									if(keus10 == "2"){
+ 										alert("De overval gaat uitstekend en je verlaat het gebouw met 200.000,- aan juwelen.")
+ 										alert("Je bent moe en je wilt naar huis je bestelt een taxi en je wordt thuis weer afgezet.")
+ 										var keus11 = prompt("1 Je glipt naar binnen en je gaat stiekem in je bed liggen. 2 Je belt aan en je moeder doet open. Je laat al je aanwinsten zien en ze is blij.")
+
+ 										if(keus11 == "1"){
+ 											alert("Ze heeft niks door en je gaat lekker slapen!")
+ 						
+
+
+ 										} else { 
+ 											// else keus 11
+ 											alert("Terwijl je binnen bent staat je vader klaar met de vaat dat je allemaal nog moet afdrogen. Je zegt dat je het niet gaat doen en ze sluiten je op in het kolenhok. Je leeft je hele leven in het hok.")
+										}
+ 									} else { 
+ 										//else keus 10
+ 										alert("Terwijl je langdurig naar juwelen kijkt arriveert de politie en je wordt meegenomen naar het politie bureau.")
+ 									}
+ 								} else {
+ 									// else keus 9
+ 									alert("Je zag niet dat er een meertje naast het grasveld zat. Er is veel wind en je land op het water. Je kan niet uitstappen en je verdrinkt.")
+ 								}	
+ 							} else {
+ 								// else keus 8
+ 								alert("Terwijl je de trap afloopt val je naar voren en je breekt meerdere botten.")
+ 							}
+ 						} else {
+ 							// else keus 7
+ 							alert("Je trekt en grappige outfit aan en je ziet een krat staan. Terwijl je het krat pakt krijg je een hernia. Erg jammer!")
+ 						}
+ 					} else { 
+ 						// else keus 6
+ 						alert("Je rijdt met de auto maar je ziet de hond niet en wijkt uit naar rechts. Je beland in het water en je verdrinkt.")
+ 					}
+ 				} else { 
+ 					// else keus 5
+ 					alert("Je was vergeten dat Ome Dirk de zwarte band heeft. Hij vindt de salade niet lekker en slaat je knock-out.")
+ 				}
+ 			} else { 
+ 				// else keus 4
+				alert("Terwijl je de man met afschuw aankijkt, word je aangereden.")
+ 			}
+ 		} else {
+ 			// else keus 3
+			alert("Je hebt een hamburger bestelt en je eet hem op. Tot je plots in een plakje augurk stikt.")
+ 		}
+ 	} else {
+ 		// else keus 2
+    	alert("Je struikelt en krijgt de steen in je oog.")
+    }
+} else {
+	// else keus 1
+	alert("Terwijl je de afwas doet glijdt je uit over water en je breekt je nek.");
+}
+
